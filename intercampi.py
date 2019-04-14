b145fdcc2e6080fd9dbc181a4ad70f9e43e2ee2f
Lucas Leite <lucasleitemm@hotmail.com>
2019-04-14 17:21:55 -0300

    inserção de  json no mongo e adição de novas rotas

--- intercampi.py
+++ intercampi.py
@@ -25,27 +25,34 @@ MONGO_PASSWORD = str(MONGO_PASSWORD)
 # accept telegram messages
 @app.route('/',methods=['POST','GET'])
 def index():
-
     if(request.method == 'POST'):
         return Response('ok',status=200)
     else:
-        client = MongoClient('e4b07ec50c05:27017', username=MONGO_USER,password=MONGO_PASSWORD)
-                     
-        db = client.admin
-        collection = db['intercampi-horario']
+        collection = get_intercampi_collection()
+        #get data from fga site
+        intercampi_dados = get_from_fga_site()
+
+       #data must exists to insert a new in mongo
+        if(len(intercampi_dados) != 0):
+            collection.delete_many({})#delete old files in collection
+            #insert each element on db    
+            for element in intercampi_dados:
+                collection.insert_one(element)
         json = []
-        for y in db['intercampi-horario'].find():
+        #find inserted elements on db
+        for y in collection.find():
             print(y['_id'])
             del y['_id']
             json.append(y)
 
-        if len(json) == 0:
-            json = get_from_fga_site()
+    return jsonify(json)
 
-        else:
-            json = json.json()
+def get_intercampi_collection():
+    client = MongoClient('e4b07ec50c05:27017', username=MONGO_USER,password=MONGO_PASSWORD)          
+    db = client.admin
+    collection = db['intercampi-horario']
 
-        return jsonify(json)
+    return collection
 
 def get_from_fga_site():
     html = get_ssl_certificate()
@@ -56,12 +63,16 @@ def get_from_fga_site():
     first_table = first_table.to_json(orient='values')
     # convert string to json
     first_table = js.loads(first_table)
-    print(first_table)
-    #todo: save on mongo before return to update
-    json = []
-    for y in first_table:
-        json.append(y)
     
+    json = []
+    #for each item,create a element and put on json list
+    for item in first_table:
+        element = {}
+        element = {'horario_saida' : item[0],'destino' : item[2] ,'origem' : item[1] }
+        json.append(element)
+
+    print(json)
+      
     return json
 
 
@@ -75,7 +86,61 @@ def get_ssl_certificate():
 
     return html
 
-# helper function to extract chat id and text
+@app.route("/darcy") ##########-----DARCY PATH-----#############
+
+def get_from_darcy():
+    collection = get_intercampi_collection()
+
+    json =[]
+    for y in collection.find():
+        if(y['origem'] == "Darcy Ribeiro"):
+            del y['_id']
+            json.append(y)
+        
+
+    return jsonify(json)
+
+@app.route("/gama") ##########-----GAMA PATH-----#############
+
+def get_from_gama():
+    collection = get_intercampi_collection()
+
+    json =[]
+    for y in collection.find():
+        if(y['origem'] == "Gama"):
+            del y['_id']
+            json.append(y)
+        
+
+    return jsonify(json)
+
+@app.route("/ceilandia") ##########-----CEILÂNDIA PATH-----#############
+
+def get_from_ceilandia():
+    collection = get_intercampi_collection()
+
+    json =[]
+    for y in collection.find():
+        if(y['origem'] == "Ceilândia"):
+            del y['_id']
+            json.append(y)
+        
+
+    return jsonify(json)
+
+@app.route("/planaltina") ##########-----PLANALTINA PATH-----#############
+
+def get_from_planaltina():
+    collection = get_intercampi_collection()
+
+    json =[]
+    for y in collection.find():
+        if(y['origem'] == "Planaltina"):
+            del y['_id']
+            json.append(y)
+        
+
+    return jsonify(json)   
 
 if(__name__ ==  '__main__'):
     app.run(debug=True, host='0.0.0.0')
