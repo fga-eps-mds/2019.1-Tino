f7a29068e234ff05d1d68f677c8ab4aefc029b89
Lucas <lucaslopes05597@gmail.com>
2019-05-10 21:50:48 -0300

    Feito exceção para  linha em branco no csv
    Co-Authored-By: Luis Bruno <l.brunofidelis@gmail.com>

--- microservices/professor.py
+++ microservices/professor.py
@@ -7,16 +7,25 @@ app = Flask(__name__)
 client = MongoClient('localhost:27017', username='rasa', password='rasa')
 db = client['admin']
 
+
 csv_professor = open('../csv/professores.csv', 'r')
+
 reader = csv.DictReader(csv_professor)
 
 collection = db['professor-contato']
 collection.delete_many({})
 
 header = ['name', 'room', 'email']
-
 for each in reader:
     row = {}
     for field in header:
-        row[field] = each[field]
+     row[field] = each[field]
+     try:
+        assert each[field] != ""
+        print("Tudo Certo ")
+       
+     except AssertionError as e:
+        print("Ha um espaco em branco.") 
+
     collection.insert_one(row)
+     
