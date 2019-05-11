19a928e26387d8f206a9dcbbad0bfeef13fde424
Lucas <lucaslopes05597@gmail.com>
2019-05-10 22:46:43 -0300

    Adicionado exceções para o arquivo csv
    Co-Authored-By: Luis Bruno <l.brunofidelis@gmail.com>

--- microservices/professor.py
+++ microservices/professor.py
@@ -2,20 +2,30 @@ from flask import Flask
 from pymongo import MongoClient
 import csv
 
-app = Flask(__name__)
+app = Flask(name)
 
+# Set db settings
 client = MongoClient('localhost:27017', username='rasa', password='rasa')
 db = client['admin']
-
-
-csv_professor = open('../csv/professores.csv', 'r')
-
-reader = csv.DictReader(csv_professor)
 
 collection = db['professor-contato']
 collection.delete_many({})
 
+try:
+    # Open CSV file in /csv directory
+    csv_professor = open('../csv/professores.csv', 'r')
+    reader = csv.DictReader(csv_professor)
+except IOError:
+    print("Houve um erro na abertura do CSV.")
+
+reader = csv.DictReader(csv_professor)
 header = ['name', 'room', 'email']
+
+# Header file validation
+if reader.fieldnames[0] != 'name' or reader.fieldnames[1] != 'room' or reader.fieldnames[2] != 'email':
+    print("O cabecalho do CSV esta incorreto.")
+
+
 for each in reader:
     row = {}
     for field in header:
