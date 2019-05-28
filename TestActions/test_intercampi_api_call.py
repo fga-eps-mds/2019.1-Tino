d5e628dbf59b244dc7cf6ad765f4c07397b31923
Lucas <lucaslopes05597@gmail.com>
2019-05-27 23:33:14 -0300

    Resolvido erro flake
    Co-authored-by: Luiz Gustavo <lgpinheiro94@gmail.com>
    
    Co-Authored-By: Lucas Leite <lucasleitemm@hotmail.com>

--- TestActions/test_intercampi_api_call.py
+++ TestActions/test_intercampi_api_call.py
@@ -3,7 +3,7 @@ import json
 from urllib import request as rq
 import os
 
-url = os.environ.get('INTERCAMPI_WEBHOOK')
+url ='https://f874fa80.ngrok.io' #os.environ.get('INTERCAMPI_WEBHOOK')
 url_darcy = url + "/darcy"
 url_gama = url + "/gama"
 url_planaltina = url + "/planaltina"
