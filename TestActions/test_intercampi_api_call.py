e7d97670b4a209428c01b1419eb28b2c7635698e
Luis Bruno <l_brunofidelis@yahoo.com.br>
2019-06-16 16:52:23 -0300

    Correção na coluna 'coordination' do CSV de professores

--- TestActions/test_intercampi_api_call.py
+++ TestActions/test_intercampi_api_call.py
@@ -3,7 +3,7 @@ import json
 from urllib import request as rq
 import os
 
-url ='https://5c50e905.ngrok.io'    #os.environ.get('INTERCAMPI_WEBHOOK')
+url ='https://73b3d842.ngrok.io'    #os.environ.get('INTERCAMPI_WEBHOOK')
 url_darcy = url + "/darcy"
 url_gama = url + "/gama"
 url_planaltina = url + "/planaltina"
